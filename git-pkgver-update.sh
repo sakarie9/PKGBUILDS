2aa0a2b4e3328f29690edabc0144b756bb0addcd
Sakari <sk308269317@outlook.com>
2023-03-03 20:52:24 +0800

    fix: clone

--- git-pkgver-update.sh
+++ git-pkgver-update.sh
@@ -4,15 +4,16 @@ process_pkgbuild() {
   source PKGBUILD
 
   url=$(echo "${source[0]}" | grep "git+" | cut -d '+' -f 2)
-  folder=$(echo "${source[0]}" | awk -F'[:]' '{print $1}')
 
   if [ -n "$url" ]; then
-    git clone "$url" "$folder-tmp"
+    git clone "$url" "$_pkgname"
   else
     echo "No git URL found in source variable."
   fi
 
   sed -i "s/pkgver=.*/pkgver=$(pkgver)/g" PKGBUILD
+
+  rm -rf $_pkgname
 }
 
 for dir in $(find "pkgbuilds" -type d -name "*-git"); do
