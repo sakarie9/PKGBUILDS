d74a8ecf3fb96ec66833a8708afcae09709ef7d1
Sakari <sk308269317@outlook.com>
2023-03-03 20:41:48 +0800

    feat: add pkgver edit script

---
+++ git-pkgver-update.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+process_pkgbuild() {
+  source PKGBUILD
+
+  url=$(echo "${source[0]}" | grep "git+" | cut -d '+' -f 2)
+  folder=$(echo "${source[0]}" | awk -F'[:]' '{print $1}')
+
+  if [ -n "$url" ]; then
+    git clone "$url" "$folder"
+  else
+    echo "No git URL found in source variable."
+  fi
+
+  sed -i "s/pkgver=.*/pkgver=$(pkgver)/g" PKGBUILD
+}
+
+for dir in $(find "pkgbuilds" -type d -name "*-git"); do
+
+  pkgbuild_file="$dir/PKGBUILD"
+  if [ -f $pkgbuild_file ]; then
+    cd $dir
+    process_pkgbuild
+  fi
+
+done
+
