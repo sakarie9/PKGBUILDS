73f816345026c107e8ec3324beafa866fddd7ec9
Sakari <sk308269317@outlook.com>
2023-04-06 17:40:13 +0800

    update: pkgver-update

--- git-pkgver-update.sh
+++ git-pkgver-update.sh
@@ -2,32 +2,8 @@
 
 process_pkgbuild() {
   cd $1
-  source PKGBUILD
-
-  if declare -p "$source" &>/dev/null; then
-    eval "_source=\${${source}[0]}"
-  else
-    _source="${source}"
-  fi
-
-  echo $_source
-
-  url=$(echo "${source}" | cut -d '+' -f 2 | cut -d '#' -f 1)
-  branch=$(echo "${source}" | grep 'branch' | cut -d '#' -f 2 | cut -d '=' -f 2)
-
-  if [ -n "$url" ]; then
-    if [ -n "$branch" ]; then
-      git clone "$url" "$_pkgname" -b "$branch"
-    else
-      git clone "$url" "$_pkgname"
-    fi
-    sed -i "s/pkgver=.*/pkgver=$(pkgver)/g" PKGBUILD
-    rm -rf $_pkgname
-    cd ../..
-  else
-    echo "No git URL found in source variable."
-    cd ..
-  fi
+  makepkg -o
+  find . -maxdepth 1 ! -name PKGBUILD -exec rm -r {} \;
 }
 
 for dir in $(find "pkgbuilds" -type d -name "*-git"); do
