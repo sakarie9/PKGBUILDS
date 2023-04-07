4bb863a1829f9855f4c9ef78783af87fd82fabdf
Sakari <sk308269317@outlook.com>
2023-04-07 17:13:52 +0800

    feat: auto cleanup

---
+++ package-cleanup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+REPO_NAME=$1 # sakari-aur
+REPO_DIR=$2 # arch-repo/repo
+DND="DO_NOT_DELETE"
+
+cd $REPO_DIR
+
+if [ ! -d $DND ]; then
+	mkdir $DND
+fi
+
+mv $(
+	tar tf $REPO_NAME.db.tar.gz |
+		awk -F/ 'NF==2{print $1}' |
+		sort -u |
+		while read line; do
+			ls $line*
+		done
+) -t $DND
+
+mv $REPO_NAME.db $REPO_NAME.db.tar.gz $REPO_NAME.files $REPO_NAME.files.tar.gz -t $DND
+
+ls | grep -v $DND | xargs rm -rfv
+
+mv $DND/* .
+
+rmdir $DND
+
+
