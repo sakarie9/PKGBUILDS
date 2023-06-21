db05d74aa7cc180633c4eb2fc7cd7967b983c946
Sakari <sk308269317@outlook.com>
2023-06-21 11:02:11 +0800

    feat: x86_64_v3 arch

--- aur-patches/clash-meta/patch.sh
+++ aur-patches/clash-meta/patch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sed -i 's/GOOS=linux CGO_ENABLED=0/GOOS=linux CGO_ENABLED=0 GOAMD64=v3/g' $1
+sed -i 's/arch=("x86_64" '\''aarch64'\'')/arch=(x86_64_v3)/g' $1
 echo "=== $1 ==="
 cat $1
 echo "=== $1 ==="
