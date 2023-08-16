02786bcb3673254c4a60c7c2fff13b8d2d997a9e
Sakari <sk308269317@outlook.com>
2023-08-16 16:55:06 +0800

    fix: actions

--- git-clone-releases.sh
+++ git-clone-releases.sh
@@ -3,13 +3,13 @@
 ACTION_REPO=$1
 ACTION_PATH=$2
 
-response=$(curl -s https://api.github.com/repos/$ACTION_REPO/releases)
+response=$(curl -s https://api.github.com/repos/$ACTION_REPO/releases/latest)
 if [[ $response == *"\"message\": \"Not Found\""* ]]; then
     echo "Release not found."
     mkdir -p $ACTION_PATH
 else
     echo "Release found."
-    curl -s "https://api.github.com/repos/$ACTION_REPO/releases" \
+    curl -s "https://api.github.com/repos/$ACTION_REPO/releases/latest" \
     | grep "browser_download_url" \
     | cut -d : -f 2,3 \
     | tr -d \" \
