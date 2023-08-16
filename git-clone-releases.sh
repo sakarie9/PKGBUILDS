0e7d484e079e8fc5bf87227fe3bd46b2a7d857bc
Sakari <sk308269317@outlook.com>
2023-08-16 11:55:47 +0800

    fix: actions

--- git-clone-releases.sh
+++ git-clone-releases.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-REPO=sakarie9/arch-repo
-REPO=sakarie9/kikitoru
-REPO=$1
-PATH=$2
+ACTION_REPO=$1
+ACTION_PATH=$2
 
-curl -s "https://api.github.com/repos/$REPO/releases/latest" \
+curl -s "https://api.github.com/repos/$ACTION_REPO/releases/latest" \
 | grep "browser_download_url" \
 | cut -d : -f 2,3 \
 | tr -d \" \
-| wget -nv -P $2 -i -
+| wget -nv -P ${ACTION_PATH} -i -
