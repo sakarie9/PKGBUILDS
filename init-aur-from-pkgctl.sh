718959e4b231f0523bd0d63ddba939b2b270fa07
Sakari <sk308269317@outlook.com>
2023-10-14 19:07:47 +0800

    [skip ci]fix

--- init-aur-from-pkgctl.sh
+++ init-aur-from-pkgctl.sh
@@ -2,6 +2,13 @@
 
 packages=(telegram-desktop)
 
+URL_BASE=https://gitlab.archlinux.org/archlinux/packaging/packages
+
 cd aur-pkgbuilds
 
-pkgctl repo clone $packages
+git clone 
+
+for(( i=0;i<${#array[@]};i++)) do
+    URL_FULL=$URL_BASE/${array[i]}.git
+    git clone $URL_FULL
+done
