73045d6034baabd8c153587787381265b221d687
Sakari <20642596+sakarie9@users.noreply.github.com>
2024-02-29 14:09:08 +0800

    add: git-clone-releases

---
+++ git-clone-releases.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ACTION_REPO=$1
+ACTION_PATH=$2
+
+response=$(curl -s https://api.github.com/repos/$ACTION_REPO/releases/latest)
+if [[ $response == *"\"message\": \"Not Found\""* ]]; then
+    echo "Release not found."
+    mkdir -p $ACTION_PATH
+else
+    echo "Release found."
+    curl -s "https://api.github.com/repos/$ACTION_REPO/releases/latest" \
+    | grep "browser_download_url" \
+    | cut -d : -f 2,3 \
+    | tr -d \" \
+    | wget -nv -P ${ACTION_PATH} -i -
+fi
